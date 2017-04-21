136eda5adf62ec62e180511fb11481f4f9e7cee1
sdelamo <sergio.delamo@softamo.com>
2017-04-21 16:57:18 +0200

    Add user

--- grails-app/init/demo/BootStrap.groovy
+++ grails-app/init/demo/BootStrap.groovy
@@ -3,6 +3,17 @@ package demo
 class BootStrap {
 
     def init = { servletContext ->
+
+        final boolean flush = true
+        final boolean failOnError = true
+        def role = new Role(authority: 'ROLE_DETECTIVE')
+        role.save(flush: flush, failOnError: true)
+        def user = new User(username: 'sherlock', password: 'elementary')
+        user.save(flush: flush)
+        def userRole = new UserRole(role: role, user: user)
+        userRole.save(flush: flush, failOnError: failOnError)
+
+
     }
     def destroy = {
     }
