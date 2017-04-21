c2cceb3c879c5dd63f420fcc2c3e3407137fbc8e
sdelamo <sergio.delamo@softamo.com>
2017-04-21 17:00:56 +0200

    s2-create-role-hierarchy-entry demo.RoleHierarchyEntry

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -26,3 +26,6 @@ grails.plugin.springsecurity.filterChain.chainMap = [
 	[pattern: '/**',             filters: 'JOINED_FILTERS']
 ]
 
+
+grails.plugin.springsecurity.roleHierarchyEntryClassName = 'demo.RoleHierarchyEntry'
+
